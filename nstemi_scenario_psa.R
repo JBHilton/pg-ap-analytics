24d7e90143ada28cb3883c2d5155e999511f8447
Joe Hilton <joe.b.hilton@gmail.com>
2025-10-08 20:01:14 +0100

    Reverted change to lognormal parameter distributions

--- nstemi_scenario_psa.R
+++ nstemi_scenario_psa.R
@@ -187,15 +187,6 @@ uncertainty_df <- read_xlsx("data-inputs/NSTEMI_masterfile_160725.xlsm",
          par1 = as.numeric(par1),
          par2 = as.numeric(par2)) # Convert values from characters to numbers
 
-# Corrections to lognormal distributions
-uncertainty_df <- uncertainty_df %>%
-  mutate(par1 = ifelse(distribution=="lognormal",
-                       yes = log(Value^2 / sqrt(Value^2 + SE^2)),
-                       no = par1),
-         par2 = ifelse(distribution=="lognormal",
-                       yes = log(1 + SE^2 / Value^2),
-                       no = par2))
-
 # Reorder so that when we take common lines with parameter dataframe everything
 # is in the correct row:
 uncertainty_df <- uncertainty_df[order(match(uncertainty_df$variable.name,
