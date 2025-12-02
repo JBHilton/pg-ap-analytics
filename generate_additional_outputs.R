bc45c891d2d8127bd2991cccabeb15f17ed5d12c
Joe Hilton <joe.b.hilton@gmail.com>
2025-12-02 10:37:22 +0000

    Updates to additional output formatting

--- generate_additional_outputs.R
+++ generate_additional_outputs.R
@@ -7,6 +7,23 @@ dir.create("additional_outputs",
 library(stringr)
 library(tidyverse)
 
+# Useful formatting function
+print_ci <- function(m,
+                     l,
+                     u,
+                     decimals=2){
+  paste(format(round(m, decimals),
+               nsmall = decimals),
+        " (",
+        format(round(l, decimals),
+               nsmall = decimals),
+        ", ",
+        format(round(u, decimals),
+               nsmall = decimals),
+        ")",
+        sep="")
+}
+
 n_sample <- 1e5
 
 stemi_samples <- read.csv("outputs/stemi_n_1e+05_psa_samples.csv")
@@ -49,6 +66,26 @@ extra_stemi_outputs <- data.frame(pop_inc_life_years_mean = summarise(stemi_samp
                                   inc_death_mc_L = summarise(stemi_samples[1:n_sample, ], q = quantile((death_mc_sc - death_mc_pc), 0.025))$q,
                                   inc_death_mc_U = summarise(stemi_samples[1:n_sample, ], q = quantile((death_mc_sc - death_mc_pc), 0.975))$q)
 
+stemi_df <- data.frame(var = colnames(extra_stemi_outputs),
+                       value = as.numeric(as.vector(extra_stemi_outputs[1, ]))) %>%
+  mutate(direction = ifelse(grepl("_mean", var),
+                            yes = "mean",
+                            no = ifelse(grepl("_L", var),
+                                        yes = "L",
+                                        no = "U"))) %>%
+  mutate(var = var %>%
+           str_replace_all("_mean|_L|_U",
+                           "")) %>%
+  pivot_wider(names_from = direction,
+              values_from = value) %>%
+  mutate(decimals = ifelse(grepl("nmb", var),
+                           yes = 0,
+                           no = 2)) %>%
+  mutate(estimate = sapply(1:length(var), FUN = function(i) print_ci(mean[i], L[i], U[i], decimals[i]))) %>%
+  select(var, estimate)
+write.csv(stemi_df,
+          file = "formatted_outputs/additional_outputs_stemi.csv")
+
 
 extra_nstemi_outputs <- data.frame(pop_inc_life_years_mean = summarise(nstemi_samples[1:n_sample, ], mean = mean(nstemi_size * (life_years_pc - life_years_sc)))$mean,
                                   pop_inc_life_years_L = summarise(nstemi_samples[1:n_sample, ], q = quantile((nstemi_size * (life_years_pc - life_years_sc)), 0.025))$q,
@@ -83,3 +120,23 @@ extra_nstemi_outputs <- data.frame(pop_inc_life_years_mean = summarise(nstemi_sa
                                   inc_death_mc_mean = summarise(nstemi_samples[1:n_sample, ], mean = mean(death_mc_sc - death_mc_pc))$mean,
                                   inc_death_mc_L = summarise(nstemi_samples[1:n_sample, ], q = quantile((death_mc_sc - death_mc_pc), 0.025))$q,
                                   inc_death_mc_U = summarise(nstemi_samples[1:n_sample, ], q = quantile((death_mc_sc - death_mc_pc), 0.975))$q)
+
+nstemi_df <- data.frame(var = colnames(extra_nstemi_outputs),
+                       value = as.numeric(as.vector(extra_nstemi_outputs[1, ]))) %>%
+  mutate(direction = ifelse(grepl("_mean", var),
+                            yes = "mean",
+                            no = ifelse(grepl("_L", var),
+                                        yes = "L",
+                                        no = "U"))) %>%
+  mutate(var = var %>%
+           str_replace_all("_mean|_L|_U",
+                           "")) %>%
+  pivot_wider(names_from = direction,
+              values_from = value) %>%
+  mutate(decimals = ifelse(grepl("nmb", var),
+                           yes = 0,
+                           no = 2)) %>%
+  mutate(estimate = sapply(1:length(var), FUN = function(i) print_ci(mean[i], L[i], U[i], decimals[i]))) %>%
+  select(var, estimate)
+write.csv(stemi_df,
+          file = "formatted_outputs/additional_outputs_nstemi.csv")
